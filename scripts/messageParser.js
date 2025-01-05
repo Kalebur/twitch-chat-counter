b4960a6ebea7586e35f0e5d6bb7e623fb72b4803
James Smith <kaleburdev@gmail.com>
2025-01-05 01:26:25 -0500

    Fix dms triggering for all messages containing the @ symbol

--- scripts/messageParser.js
+++ scripts/messageParser.js
@@ -49,5 +49,5 @@ function isValidMessage(message) {
 }
 
 function isMessageDirectedAtMonitoredUser(messageNode) {
-  return messageNode.innerText.toLowerCase().includes(`@`);
+  return messageNode.innerText.toLowerCase().includes(`@${userToMonitor}`);
 }
